ea3e1bd5fde3fad6fceacc48403701ff586daee3
undynx <martinezf.maite@gmail.com>
2022-10-13 20:45:04 -0300

    agregue validaciones antes de crear una columna

--- columna.c
+++ columna.c
@@ -10,49 +10,28 @@
 
 using namespace std;
 
-/*
-//Definicion de Tipos de dato
-tipoInt[3] = 'INT';
-tipoStr[3] = 'STR';
-
-//Definion de Calificador
-califNotE = 'not empty';
-califPrimKey
-*/
-
 struct columna{
-	int indice;
 	char * nombreColumna;
-	char * tipo;
+	TipoDatoCol * tipo;
 	char * datoStr;
 	int datoInt;
-	char * calif;
+	Calificador * calif;
 	columna sig;
 };
-/*
-struct columnaString{
-	int indice;
-	char nombreColumna;
-	char dato;
-	columnaString sig;
-	Calificador
-};
-*/
 
-void addCol(tabla &t, char * nombreColumna, char tipoDato, char calificador){
+tipoRet agregarCol(tabla &t, char * nombreColumna, TipoDatoCol tipoDato, Calificador calificador){
 	
 	columna col = new(nodo_columna);
-	//asigna el nombre a la columna
-	col->nombreColumna = new char[20];
-	strcpy(t->nombreColumna, nombreColumna); 
-	//asigna el tipo
-	col->tipo = new char[6];
-	strcpy(t->tipo, tipoDato);
-	//asigna el calificador
 	
-	//TO-DO revisar si los to-dos siguientes van aca o van en tabla.h
-	//TO-DO validar si la tabla es vacia antes de asignar el calificador, si la tabla no es vacia no puede ser algo que no sea ANY
-	//TO-DO validar si existe la tabla
-	col->calif = new char[11];
-	strcpy(t->calif, calificador);
+	//Asigna el nombre a la columna
+	col->nombreColumna = new char[20]; 
+	strcpy(t->nombreColumna, nombreColumna); 
+	
+	//Asigna el tipo
+	col->tipo = new(TipoDatoCol);
+	col->tipo = tipoDato;
+	
+	//Asigna el calificador
+	col->calif = new(Calificador);
+	col->calif = calificador;
 };
