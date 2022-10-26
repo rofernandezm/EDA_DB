98786c86686943be3f7265267210848ed2e78269
undynx <martinezf.maite@gmail.com>
2022-10-25 21:07:55 -0300

    funcion insertInto, solo Dios y yo sabemos como funciona

--- celda.h
+++ celda.h
@@ -16,4 +16,16 @@ void eliminarCeldas(celda &celda);
 //Elimina las celdas en una determinada columna
 //Pre: celda != NULL
 
+bool existeDato_char(celda celda, char * dato);
+//Devuelve true si en una columna de tipo char existe el dato
+
+bool existeDato_int(celda celda, int dato);
+//Devuelve true si en una columna de tipo int existe el dato
+
+void insertInto_char(celda celda, char * dato);
+//Inserta en una columna tipo char
+
+void insertInto_int(celda celda, int dato);
+//Inserta en una columna tipo int
+
 #endif
